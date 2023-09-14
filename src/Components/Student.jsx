de80a599911f8b47ed7bd33aa80273cd88f06a75
BrandonSGC <brangmz20@gmail.com>
2023-09-13 21:34:38 -0600

    New styles student buttons

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -9,7 +9,7 @@ export const Student = ({ studentData }) => {
       <td>{name}</td>
       <td>{surname}</td>
       <td>{subject}</td>
-      <td className="buttons">
+      <td className="buttons-student">
         <button className="button-delete">Delete</button>
         <button className="button-edit">Edit</button>
       </td>
